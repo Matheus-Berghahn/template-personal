e5d59f3e13a4614913a4ec37264c262464415978
Matheus Berghahn <matheus.berghahn3k@gmail.com>
2025-02-28 17:08:48 -0300

    pagian finalizada

--- template/src/app/components/Marquee.tsx
+++ template/src/app/components/Marquee.tsx
@@ -1,10 +1,21 @@
 
 import { motion } from "framer-motion";
-import {  useState } from "react";
+import { useEffect, useState } from "react";
 
 const MarqueeSection = () => {
-  const [scrollY] = useState(0);
+  const [scrollY, setScrollY] = useState(0);
 
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollY(window.scrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   const moveText = (scrollY: number) => {
     const slowSpeed = 0.01;
